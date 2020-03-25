d10670beb34ce792b7f5d63beea47276ccbacd15
Marco Fiorito <marco@xmartlabs.com>
2020-03-25 00:00:45 -0300

    react-navigation setup

---
+++ src/scenes/home/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/style/Palette.ts
@@ -0,0 +1,8 @@
+export const Palette = {
+  background: '#f8f8f8',
+  black: '#000000',
+  clearBlue: '#1f86ff',
+  gray: '#959595',
+  separator: '#f0f0f0',
+  white: '#ffffff',
+};
